8e8a998dc5304c2177b5571bb6ff69762183d72c
Hamraz <hamriouz@gmail.com>
2023-09-05 16:09:26 +0330

    Remove optional.

--- src/main/kotlin/com/example/WalletManager/repository/WalletRepository.kt
+++ src/main/kotlin/com/example/WalletManager/repository/WalletRepository.kt
@@ -3,12 +3,11 @@ package com.example.WalletManager.repository
 import com.example.WalletManager.model.Wallet
 import org.springframework.stereotype.Repository
 import org.springframework.data.repository.CrudRepository
-import java.util.Optional
 
 
 @Repository
 interface WalletRepository : CrudRepository<Wallet, Int> {
-    fun findByUserId(userId: Int): Optional<Wallet>?
+    fun findByUserId(userId: Int): Wallet?
 }
 
 
